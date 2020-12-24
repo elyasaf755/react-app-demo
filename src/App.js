40d05287686e4140fc48e8f1c017224e51b8b784
Elyasaf Elbaz <47217505+elyasaf755@users.noreply.github.com>
2020-12-25 00:40:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Deployed by Elyasaf755</p>
+        <p>'Sup Yishai?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
